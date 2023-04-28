b38d3a5e5eaac755b47e5aaf134dbf7b41c02a84
Sive Mdluli <mdlulisive202@gmail.com>
2023-04-28 16:36:24 +0200

    Fix linter erros in css and js

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
 // Dynamic speakers
-  const speakers = [
-    {
-      image: 'img/speakers/speaker1.png',
-      name: 'Yochai Bankler',
-      occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
-    },
-    {
-      image: 'img/speakers/speaker2.png',
-      name: 'Yochai Bankler',
-      occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
-    },
-    {
-      image: 'img/speakers/speaker3.png',
-      name: 'Yochai Bankler',
-      occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
-    },
-    {
-      image: 'img/speakers/speaker4.png',
-      name: 'Yochai Bankler',
-      occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
-    },
-    {
-      image: 'img/speakers/speaker5.png',
-      name: 'Yochai Bankler',
-      occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
-    },
-    {
-      image: 'img/speakers/speaker6.png',
-      name: 'Yochai Bankler',
-      occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
-    },
-  ];
+const speakers = [
+  {
+    image: 'img/speakers/speaker1.png',
+    name: 'Yochai Bankler',
+    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
+  },
+  {
+    image: 'img/speakers/speaker2.png',
+    name: 'Yochai Bankler',
+    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
+  },
+  {
+    image: 'img/speakers/speaker3.png',
+    name: 'Yochai Bankler',
+    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
+  },
+  {
+    image: 'img/speakers/speaker4.png',
+    name: 'Yochai Bankler',
+    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
+  },
+  {
+    image: 'img/speakers/speaker5.png',
+    name: 'Yochai Bankler',
+    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
+  },
+  {
+    image: 'img/speakers/speaker6.png',
+    name: 'Yochai Bankler',
+    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
+  },
+];
 
-  const appendFeatured = document.getElementById('featured-speakers');
+const appendFeatured = document.getElementById('featured-speakers');
 
-  const image = speakers.map((speak) => speak.image);
+const image = speakers.map((speak) => speak.image);
 
-  const name = speakers.map((speak) => speak.name);
+const name = speakers.map((speak) => speak.name);
 
-  const occupation = speakers.map((speak) => speak.occupation);
+const occupation = speakers.map((speak) => speak.occupation);
 
-  const description  = speakers.map((speak) => speak.description);
+const description = speakers.map((speak) => speak.description);
 
-  appendFeatured.insertAdjacentHTML('afterbegin', 
-  '<div class="fs-header"> <h2>Featured Speakers</h2> <hr class="hr-line"> </div>'
-  );
+appendFeatured.insertAdjacentHTML('afterbegin',
+  '<div class="fs-header"> <h2>Featured Speakers</h2> <hr class="hr-line"> </div>');
 
-  for(let i = 0; i < speakers.length; i+=1) {
-    appendFeatured.insertAdjacentHTML('beforeend', `
+for (let i = 0; i < speakers.length; i += 1) {
+  appendFeatured.insertAdjacentHTML('beforeend', `
       <div class="fs-content"> 
           <div class="speaker">
             <img src="${image[i]}" alt="Speaker 1" class="speaker-img">
@@ -70,6 +69,4 @@
           </div>
         </div>
     `);
-  };
-
-
+}
